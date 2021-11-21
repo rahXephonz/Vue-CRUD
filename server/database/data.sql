69fb35974789ac5cb63887c311f1677b3a3a909d
ioofy <mynev.id@gmail.com>
2021-11-21 22:16:57 +0700

    modified a database relations and create a docs about CRUD methods, adding router

---
+++ server/database/data.sql
@@ -0,0 +1,33 @@
+--CREATE A DATABASE
+
+CREATE DATABASE department;
+
+--CREATE A TABLES
+
+CREATE TABLE IF NOT EXISTS employees (
+    id serial,
+    name VARCHAR(50) NOT NULL,
+    gender VARCHAR(7) NOT NULL.
+    departement VARCHAR(50) NOT NULL,
+    about TEXT,
+    PRIMARY KEY(id)
+);
+
+--INSERT DATA (CREATE)
+INSERT INTO employees (name, gender, departement, about) VALUES ('Valerian Trifa', 'Male', 'FrontEnd Developer', 'He is a senior frontend developer from kata.ai')
+
+--GET ALL DATA (READ)
+SELECT * FROM employees
+
+--GET DATA FROM SPECIFIC
+SELECT * FROM employees WHERE id = {variable}
+
+--UPDATE DATA (UPDATE)
+UPDATE employees SET name = 'New Name', gender = 'New Gender', departement = 'New Departement', about = 'New About'
+        WHERE id = {variable} RETURNING *
+
+--DELETE DATA
+DELETE FROM employees WHERE id = {variable}
+
+--RESET INCREMENT VARIABLE ID
+TRUNCATE TABLE someTable RESTART IDENTITY;
---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express();
+const PORT = 5000
+const createEmployeeTable = require('./database/EmployeeStorage')
+const employees = require('./router/EmployeesRouter')
+
+
+app.get('/', async(req, res) => {
+    res.send(`API is running in... (>‿◠)✌`)
+})
+
+//akan dijalankan tidak pedulin itu http method,put,delete app use akan tetap dijalankan dan tidak butuh route
+//dan mengubah data client dalam bentuk json
+app.use(express.json())
+app.use('/api/employees', employees)
+
+
+app.listen(PORT, async () => {
+    try {
+        await createEmployeeTable()
+        console.log(`LISTENING PORT IN ${PORT}`)
+    } catch (error) {
+        console.error(error)
+    }
+})
