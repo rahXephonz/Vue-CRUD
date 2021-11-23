684672190e2692b8d9ae7b79a9617ac19a727d76
ioofy <mynev.id@gmail.com>
2021-11-23 21:39:35 +0700

    Adding client side for frontend, adding a vue app with tailwind config

---
+++ client/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <SideBar />
+    <v-main>
+      <router-view />
+    </v-main>
+  </div>
+</template>
+
+<script>
+import SideBar from '@/components/SideBar.vue';
+export default {
+  name: 'App',
+  components: {
+    SideBar,
+  },
+};
+</script>
