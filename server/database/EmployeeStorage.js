ea75bab9e96bcde5e9066dd3c17fc2c67f6ebcb8
ioofy <mynev.id@gmail.com>
2021-11-23 21:40:08 +0700

    adding prettier for styling code

--- server/database/EmployeeStorage.js
+++ server/database/EmployeeStorage.js
@@ -1,22 +1,21 @@
-const db = require('./index')
-const TABLE_NAME = 'employees'
+const db = require('./index');
+const TABLE_NAME = 'employees';
 
-async function createEmployeeTable(){    
-    try {
-        console.log('EMPLOYEE TABLE CREATED')
-        let sql = `CREATE TABLE IF NOT EXISTS ${TABLE_NAME}(
-            id serial,
-            name VARCHAR(50) NOT NULL,
-            gender VARCHAR(7) NOT NULL,
-            departement VARCHAR(50) NOT NULL,
-            about text,
-            PRIMARY KEY(id)
-        )`
-        await db.query(sql)
-        
-    } catch (error) {
-        console.error(error)
-    }
+async function createEmployeeTable() {
+  try {
+    console.log('EMPLOYEE TABLE CREATED');
+    let sql = `CREATE TABLE IF NOT EXISTS ${TABLE_NAME}(
+                id serial,
+                name VARCHAR(50) NOT NULL,
+                gender VARCHAR(7) NOT NULL,
+                departement VARCHAR(50) NOT NULL,
+                about text,
+                PRIMARY KEY(id)
+        )`;
+    await db.query(sql);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-module.exports = createEmployeeTable
+module.exports = createEmployeeTable;
