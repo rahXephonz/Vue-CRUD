557b43e53e47810a8c4bd4abd1709976c2f61a13
ioofy <mynev.id@gmail.com>
2021-11-24 22:11:50 +0700

    update: adding CORS for integration and consume it in frontend

--- server/index.js
+++ server/index.js
@@ -1,25 +1,26 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 const app = express();
-const PORT = 5000
-const createEmployeeTable = require('./database/EmployeeStorage')
-const employees = require('./router/EmployeesRouter')
+const PORT = 5000;
+const createEmployeeTable = require('./database/EmployeeStorage');
+const employees = require('./router/EmployeesRouter');
 
+app.get('/', async (req, res) => {
+  res.send(`API is running in... (>‿◠)✌`);
+});
 
-app.get('/', async(req, res) => {
-    res.send(`API is running in... (>‿◠)✌`)
-})
+app.use(cors());
 
 //akan dijalankan tidak pedulin itu http method,put,delete app use akan tetap dijalankan dan tidak butuh route
 //dan mengubah data client dalam bentuk json
-app.use(express.json())
-app.use('/api/employees', employees)
-
+app.use(express.json());
+app.use('/api/employees', employees);
 
 app.listen(PORT, async () => {
-    try {
-        await createEmployeeTable()
-        console.log(`LISTENING PORT IN ${PORT}`)
-    } catch (error) {
-        console.error(error)
-    }
-})
+  try {
+    await createEmployeeTable();
+    console.log(`LISTENING PORT IN ${PORT}`);
+  } catch (error) {
+    console.error(error);
+  }
+});
