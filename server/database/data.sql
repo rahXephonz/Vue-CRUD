2463265e9bd5982ad8464c6b23df12ace5c9cd9d
ioofy <mynev.id@gmail.com>
2021-11-25 22:17:03 +0700

    update: adding a password, email query for login and editing a router API

--- server/database/data.sql
+++ server/database/data.sql
@@ -7,8 +7,10 @@ CREATE DATABASE department;
 CREATE TABLE IF NOT EXISTS employees (
     id serial,
     name VARCHAR(50) NOT NULL,
-    gender VARCHAR(7) NOT NULL.
+    gender VARCHAR(7) NOT NULL,
     departement VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
     about TEXT,
     PRIMARY KEY(id)
 );
--- server/router/EmployeesRouter.js
+++ server/router/EmployeesRouter.js
@@ -1,73 +1,85 @@
-const express = require('express')
-const router = express.Router()
-const db = require('../database/index')
+const express = require('express');
+const router = express.Router();
+const db = require('../database/index');
 
 //CREATE DATA
-router.post('/', async(req, res, next) => {
-    try {
-        const employee = req.body
-        const sqlText = `INSERT INTO employees (name, gender, departement, about) VALUES ($1, $2, $3, $4) RETURNING *`
-        const sqlParams = [employee.name, employee.gender, employee.departement, employee.about]
-        const result = (await db.query(sqlText, sqlParams)).rows
-        res.json(result)
-    } catch (error) {
-        console.error(error)
-        next(error)
-    }
-})
+router.post('/', async (req, res, next) => {
+  try {
+    const employee = req.body;
+    const sqlText = `INSERT INTO employees (name, gender, departement, about, email, password) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`;
+    const sqlParams = [
+      employee.name,
+      employee.gender,
+      employee.departement,
+      employee.about,
+      employee.email,
+      employee.password,
+    ];
+    const result = (await db.query(sqlText, sqlParams)).rows;
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 //UPDATE DATA
-router.put('/:id', async(req, res, next) => {
-    try {
-        const employee = req.body
-        const sqlText = `UPDATE employees SET name = $1, gender = $2, departement = $3, about = $4
-        WHERE id = $5 RETURNING *`
-        const sqlParams = [employee.name, employee.gender, employee.departement, employee.about, req.params.id]
-        const result = (await db.query(sqlText, sqlParams)).rows
-        res.json(result)
-    } catch (error) {
-        console.error(error)
-        next(error)
-    }
-})
+router.put('/:id', async (req, res, next) => {
+  try {
+    const employee = req.body;
+    const sqlText = `UPDATE employees SET name = $1, gender = $2, departement = $3, about = $4, email = $5, password = $6
+        WHERE id = $7 RETURNING *`;
+    const sqlParams = [
+      employee.name,
+      employee.gender,
+      employee.departement,
+      employee.about,
+      req.params.id,
+    ];
+    const result = (await db.query(sqlText, sqlParams)).rows;
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 //READ DATA
-router.get('/', async(req, res, next) => {
-    try {
-        let sql = `SELECT * FROM employees`
-        let result = (await db.query(sql)).rows
-        res.send(result)
-    } catch (error) {
-        console.error(error)
-        next(error)
-    }
-})
+router.get('/', async (req, res, next) => {
+  try {
+    let sql = `SELECT * FROM employees`;
+    let result = (await db.query(sql)).rows;
+    res.send(result);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 //DELETE DATA
-router.delete('/:id', async(req, res, next) => {
-    try {
-        const sqlText = `DELETE FROM employees WHERE id = $1 RETURNING *`
-        const sqlParams = [req.params.id]
-        const result = (await db.query(sqlText, sqlParams)).rows
-        res.json(result)
-
-    } catch (error) {
-        console.error(error)
-        next(error)
-    }
-})
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const sqlText = `DELETE FROM employees WHERE id = $1 RETURNING *`;
+    const sqlParams = [req.params.id];
+    const result = (await db.query(sqlText, sqlParams)).rows;
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 //GET DATA FROM SPECIFIC ID
-router.get('/:id', async(req, res, next) => {
-    try {
-        const sqlText = `SELECT * FROM employees WHERE id = $1`
-        const sqlParams = [req.params.id]
-        const result = (await db.query(sqlText, sqlParams)).rows
-        res.json(result)
-    } catch (error) {
-        console.error(error)
-        next(error)
-    }
-})
+router.get('/:id', async (req, res, next) => {
+  try {
+    const sqlText = `SELECT * FROM employees WHERE id = $1`;
+    const sqlParams = [req.params.id];
+    const result = (await db.query(sqlText, sqlParams)).rows;
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
-module.exports = router
+module.exports = router;
